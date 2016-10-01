c9c231d5756190730ddb0df5697ffd8b5d286105
Andrei Popa <andrei@alttab.co>
2016-10-01 11:27:10 +0300

    - added localizedDescription for QueryError

--- Sources/MySQL/Error.swift
+++ Sources/MySQL/Error.swift
@@ -20,4 +20,31 @@ public enum QueryError: Error {
     case missingKeyError(key: String)
     
     case invalidSQLDate(String)
+    
+    
+    public var localizedDescription : String {
+        switch self {
+            
+        case .queryExecutionError(let message, let query):
+            return "MySQL execution error: \(message), for query: \(query)"
+        case .resultFetchError(let message, let query):
+            return "MySQL result fetch error: \(message), for query: \(query)"
+        case .resultNoField(let query):
+            return "MySQL no field error: \(query)"
+        case .resultRowFetchError(let query):
+            return "MySQL row fetch error: \(query)"
+        case .resultFieldFetchError(let query):
+            return "MySQL field fetch error: \(query)"
+        case .resultParseError(let message, let result):
+            return "MySQL parse error: \(message), result: \(result)"
+        case .fieldIndexOutOfBounds(_, let attempted, let fieldName):
+            return "MySQL field index out of bounds: \(fieldName), index: \(attempted)"
+        case .castError(let actual, let expected, let key):
+            return "MySQL cast error: actual \(actual), expected: \(expected), key: \(key)"
+        case .missingKeyError(let key):
+            return "MySQL missing ley error: \(key)"
+        case .invalidSQLDate(let date):
+            return "MySQL invalid date error: \(date)"
+        }
+    }
 }
