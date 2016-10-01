d926566b34dee364734c2d27f3d3b77861d98622
Andrei Popa <andrei@alttab.co>
2016-10-01 10:28:33 +0300

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@ import PackageDescription
 
 let package = Package(
     name: "MySQL",
+    targets: [
+        Target(name: "SQLFormatter"),
+        Target(name: "MySQL", dependencies: ["SQLFormatter"])
+    ],
     dependencies: [
         .Package(url: "https://github.com/novi/CMySQL.git", majorVersion: 2)
-    ],
-    targets: [
-                 Target(name: "SQLFormatter"),
-                 Target(name: "MySQL", dependencies: ["SQLFormatter"])
-                 ]
+    ]
 )
